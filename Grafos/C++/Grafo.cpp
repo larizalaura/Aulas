6a576cd28b5ff979cab88d88ee41612c8f94ae4e
Lariza Laura de Oliveira <larizalaura@gmail.com>
2020-06-04 19:03:57 -0300

    Update Grafo.cpp

--- Grafos/C++/Grafo.cpp
+++ Grafos/C++/Grafo.cpp
@@ -4,62 +4,80 @@
 ******/
 
 #include <iostream>
+#include "Queue.h"
+#include "Grafo.h"
+
+
 using namespace std;
 
-class Grafo {
-   private:
-  bool** adjMatriz;
-  int numVertices;
 
-   public:
-  // Inicializa a matriz com zero
-  Grafo(int numVertices) {
-    this->numVertices = numVertices;
-    adjMatriz = new bool*[numVertices];
-    for (int i = 0; i < numVertices; i++) {
-      adjMatriz[i] = new bool[numVertices];
-      for (int j = 0; j < numVertices; j++)
-        adjMatriz[i][j] = false;
-    }
-  }
+// Inicializa a matriz com zero
+Grafo::Grafo(int numVertices) {
+	this->numVertices = numVertices;
+	//cout<< "num_vertices " << numVertices<< endl;
+	adjMatriz = new bool*[numVertices];
+	for (int i = 0; i < numVertices; i++) {
+		adjMatriz[i] = new bool[numVertices];
+		for (int j = 0; j < numVertices; j++){
+			adjMatriz[i][j] = false;
+		}
 
-  // Adiciona arestas
-  void addEdge(int i, int j) {
-    adjMatriz[i][j] = true;
-    adjMatriz[j][i] = true;
-  }
+	}
+}
 
-  // Remove as arestas
-  void removeEdge(int i, int j) {
-    adjMatriz[i][j] = false;
-    adjMatriz[j][i] = false;
-  }
+bool Grafo::empty(){
+	return(numVertices==0);
+}
 
+int Grafo::nVertices(){
+	return(numVertices);
+}
+//int nEdges(){
+//int size()
+//int degree(int v);
+
+// Adiciona arestas
+void Grafo::addEdge(int i, int j) {
+	adjMatriz[i][j] = true;
+	adjMatriz[j][i] = true;
+}
+
+// Remove as arestas
+void Grafo::removeEdge(int i, int j) {
+	adjMatriz[i][j] = false;
+	adjMatriz[j][i] = false;
+}
   // Printa a matriz
-  void toString() {
+void Grafo::toString() {
     for (int i = 0; i < numVertices; i++) {
       cout << i << " : ";
       for (int j = 0; j < numVertices; j++)
         cout << adjMatriz[i][j] << " ";
       cout << "\n";
     }
-  }
-
-  ~Grafo() {
-    for (int i = 0; i < numVertices; i++)
-      delete[] adjMatriz[i];
-    delete[] adjMatriz;
-  }
-};
-
-int main() {
-  Grafo g(4);
-
-  g.addEdge(0, 1);
-  g.addEdge(0, 2);
-  g.addEdge(1, 2);
-  g.addEdge(2, 0);
-  g.addEdge(2, 3);
-
-  g.toString();
+ }
+// Deep First Search (Busca em profundidade)
+void Grafo::DFS(int v, int visited[]){
+	visited[v] = 1;
+	cout<< " " << v << " "<< endl;
+	for (int w = 0; w < numVertices; w++){
+		if(adjMatriz[v][w] ==1 && visited[w] ==0)
+			DFS(w, visited);
+	}
 }
+
+void Grafo::callDFS(int v){
+	int visited[numVertices];
+	for (int i = 0; i < numVertices; i++)
+		visited[i] = 0;
+
+	DFS(v, visited);
+}
+
+Grafo::~Grafo() {
+	for (int i = 0; i < numVertices; i++)
+		delete[] adjMatriz[i];
+		delete[] adjMatriz;
+}
+
+
